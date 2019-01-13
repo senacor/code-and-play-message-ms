8245230522b3a84471862a3a0d1882c49b069404
Andri Bremm <andri.bremm@senacor.com>
2019-01-13 22:30:19 +0100

    Fix build

--- scripts/prepare_deployment_files.sh
+++ scripts/prepare_deployment_files.sh
@@ -2,9 +2,29 @@
 
 set -eu
 
+if [[ -z "$SERVICE_NAME" ]]; then
+  echo "Variable SERVICE_NAME not set!"
+  exit 1
+fi
+
+if [[ -z "$TRAVIS_BRANCH" ]]; then
+  echo "Variable TRAVIS_BRANCH not set!"
+  exit 1
+fi
+
+if [[ -z "$TRAVIS_BUILD_NUMBER" ]]; then
+  echo "Variable TRAVIS_BUILD_NUMBER not set!"
+  exit 1
+fi
+
+if [[ -z "$DOCKER_IMAGE" ]]; then
+  echo "Variable DOCKER_IMAGE not set!"
+  exit 1
+fi
+
 mkdir -p ./target/kubernetes/
 
-for file in ./deployment/*.yaml
+for file in ./kubernetes/*.yaml
 do
   filename=$(basename "$file")
   envsubst < "$file" > "./target/kubernetes/$filename"
