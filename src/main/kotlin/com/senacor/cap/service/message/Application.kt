5108d9ff4c03bb8da3b2f9e1d5a0e18c6762a7f7
rmuelle4 <roland.georg.mueller@gmail.com>
2019-01-14 13:53:07 +0100

    Update channel-ms endpoint

--- src/main/kotlin/com/senacor/cap/service/message/Application.kt
+++ src/main/kotlin/com/senacor/cap/service/message/Application.kt
@@ -46,7 +46,7 @@ class Application {
 
             val request = HttpEntity(sb.toString(), headers)
 
-            restTemplate.postForObject<String>("http://localhost:8080/api/channels", request)
+            restTemplate.postForObject<String>("http://channel-ms:8080/api/channels", request)
         }
     }
 
