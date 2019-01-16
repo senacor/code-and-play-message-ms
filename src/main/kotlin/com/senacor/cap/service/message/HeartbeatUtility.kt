5fce099a1bd212f575bbdebcc774d01513bab86a
Andri Bremm <andri.bremm@senacor.com>
2019-01-16 22:17:03 +0100

    Renamed channel name to id.

--- src/main/kotlin/com/senacor/cap/service/message/HeartbeatUtility.kt
+++ src/main/kotlin/com/senacor/cap/service/message/HeartbeatUtility.kt
@@ -11,38 +11,38 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForObject
 
+data class Channel(val id: String, val endpoint: String)
+
 @Component
-class HeartbeatUtility(val env: Environment) {
-    private val log: Logger = LoggerFactory.getLogger(Application::class.java)
+class HeartbeatUtility(env: Environment) {
+    private val log: Logger = LoggerFactory.getLogger(HeartbeatUtility::class.java)
 
-    @Scheduled(fixedDelay = 30000)
-    fun heartbeat() {
+    private val restTemplate = RestTemplate()
 
+    private lateinit var channel: String
+    private lateinit var host: String
+    private lateinit var port: String
+    private lateinit var channelMsHost: String
+    private lateinit var channelMsPort: String
 
-        val restTemplate = RestTemplate()
-        val sb = StringBuilder()
-
-        val channel = env.getProperty("SERVICE_CHANNEL")
-        val host = env.getProperty("MESSAGE_MS_" + channel!!.toUpperCase() + "_SERVICE_HOST")
-        val port = env.getProperty("MESSAGE_MS_" + channel.toUpperCase() + "_SERVICE_PORT")
-        val channelMsHost = env.getProperty("CHANNEL_MS_SERVICE_HOST")
-        val channelMsPort = env.getProperty("CHANNEL_MS_SERVICE_PORT")
+    init {
+        channel = env.getProperty("SERVICE_CHANNEL")!!
+        host = env.getProperty("MESSAGE_MS_" + channel.toUpperCase() + "_SERVICE_HOST")!!
+        port = env.getProperty("MESSAGE_MS_" + channel.toUpperCase() + "_SERVICE_PORT")!!
+        channelMsHost = env.getProperty("CHANNEL_MS_SERVICE_HOST")!!
+        channelMsPort = env.getProperty("CHANNEL_MS_SERVICE_PORT")!!
 
         log.info("Registering channel $channel with endpoint $host:$port")
         log.debug("Host address is $host:$port")
+    }
 
-        sb.append("{")
-        sb.append("\"name\":\"$channel\",")
-        sb.append("\"endpoint\":\"http://$host:$port\"")
-        sb.append("}")
-
-
+    @Scheduled(fixedDelay = 30000)
+    fun heartbeat() {
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
 
-        val request = HttpEntity(sb.toString(), headers)
+        val request = HttpEntity(Channel(channel, "http://$host:$port"), headers)
 
         restTemplate.postForObject<String>("http://$channelMsHost:$channelMsPort/api/channels", request)
-
     }
 }
