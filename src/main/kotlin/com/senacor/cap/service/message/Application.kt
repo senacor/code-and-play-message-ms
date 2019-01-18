93d67fc45131d53fab42826d5cb475f9bafeeb8a
felix <felix.kampfer@hs-augsburg.de>
2019-01-18 11:41:25 +0100

    adding ChatMessage class

--- src/main/kotlin/com/senacor/cap/service/message/Application.kt
+++ src/main/kotlin/com/senacor/cap/service/message/Application.kt
@@ -3,6 +3,7 @@ package com.senacor.cap.service.message
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.sql.Timestamp
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,3 +12,5 @@ class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
+
+class ChatMessage (channelId: String, sender: String, message: String, creationTimestamp: Timestamp, id:Int)
