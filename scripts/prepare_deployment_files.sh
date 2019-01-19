d1eb83fdcaef6521cb3fe0059d105b1275f7c807
dianasambukina <dianasambukina@hotmail.com>
2019-01-19 15:08:08 +0100

    replicas>2

--- scripts/prepare_deployment_files.sh
+++ scripts/prepare_deployment_files.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+export TRAVIS_BUILD_NUMBER=130
+export TRAVIS_BRANCH=dianasam
+export SERVICE_NAME=message-ms
+export DOCKER_IMAGE=$SERVICE_NAME:$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
+
 if [[ -z "$SERVICE_NAME" ]]; then
   echo "Variable SERVICE_NAME not set!"
   exit 1
