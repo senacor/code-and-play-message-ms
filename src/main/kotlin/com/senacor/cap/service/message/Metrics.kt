c816c40c039c1f464d5eede549c33cec63481a51
lilli <caothivananh98@gmail.com>
2019-01-19 11:53:59 +0100

    add monitoring metrics

--- src/main/kotlin/com/senacor/cap/service/message/Metrics.kt
+++ src/main/kotlin/com/senacor/cap/service/message/Metrics.kt
@@ -1,15 +1,16 @@
 package com.senacor.cap.service.message
 
 import io.micrometer.core.instrument.Metrics
+import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.Tags
 
 object Metrics {
 
     private const val prefix = "message_ms_"
 
-    internal val someCounter = Metrics.counter(prefix + "some_counter_total")
-
-    /**
-     * usage example:
-     * Metrics.someCounter.increment()
-     */
+    internal val countMsgSavedTotal = Metrics.counter(prefix + "message_ms_messages_saved_total")
+    internal val countRequestPostTotal = Metrics.counter(prefix + "message_ms_messages_requests_total",
+                                                     Tags.of(Tag.of("method", "GET")))
+    internal val countRequestGetTotal = Metrics.counter(prefix + "message_ms_messages_requests_total",
+                                                     Tags.of(Tag.of("method", "GET")))
 }
--- src/main/kotlin/com/senacor/cap/service/message/api/ChatMessageController.kt
+++ src/main/kotlin/com/senacor/cap/service/message/api/ChatMessageController.kt
@@ -1,5 +1,6 @@
 package com.senacor.cap.service.message.api
 
+import com.senacor.cap.service.message.Metrics
 import com.senacor.cap.service.message.repository.ChatMessage
 import com.senacor.cap.service.message.service.ChatMessageService
 import org.springframework.http.ResponseEntity
@@ -15,11 +16,14 @@ class ChatMessageController(val chatMessageService: ChatMessageService)
 {
     @GetMapping("/api/channels/{channelId}/messages")
     fun loadChatMessages(@PathVariable channelId: String): List<ChatMessage> {
+        Metrics.countRequestGetTotal.increment()
         return chatMessageService.loadChatMessages(channelId)
     }
 
     @PostMapping("/api/channels/{channelId}/messages")
     fun newChatMessages(@PathVariable channelId: String?, @RequestBody chatMessage: ChatMessage): ResponseEntity<ChatMessage> {
+        Metrics.countMsgSavedTotal.increment()
+        Metrics.countRequestPostTotal.increment()
         val saveChatMessage = chatMessageService.saveChatMessage(channelId, chatMessage.sender, chatMessage.message)
         val id = saveChatMessage.id;
         return ResponseEntity.created(URI("/api/channels/$channelId/messages/$id")).body(saveChatMessage)
