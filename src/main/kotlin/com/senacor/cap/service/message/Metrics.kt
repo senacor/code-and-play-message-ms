75593a88768d00fdcdd676fd0980c2467246967e
lilli <caothivananh98@gmail.com>
2019-01-19 12:12:25 +0100

    remove duplicate prefix in metrics

--- src/main/kotlin/com/senacor/cap/service/message/Metrics.kt
+++ src/main/kotlin/com/senacor/cap/service/message/Metrics.kt
@@ -8,9 +8,9 @@ object Metrics {
 
     private const val prefix = "message_ms_"
 
-    internal val countMsgSavedTotal = Metrics.counter(prefix + "message_ms_messages_saved_total")
-    internal val countRequestPostTotal = Metrics.counter(prefix + "message_ms_messages_requests_total",
+    internal val countMsgSavedTotal = Metrics.counter(prefix + "messages_saved_total")
+    internal val countRequestPostTotal = Metrics.counter(prefix + "messages_requests_total",
                                                      Tags.of(Tag.of("method", "GET")))
-    internal val countRequestGetTotal = Metrics.counter(prefix + "message_ms_messages_requests_total",
+    internal val countRequestGetTotal = Metrics.counter(prefix + "messages_requests_total",
                                                      Tags.of(Tag.of("method", "GET")))
 }
