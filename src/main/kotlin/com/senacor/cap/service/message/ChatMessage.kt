82d9d1c56928495b37b0b2e0892db64c1b95d43f
Jan <jan.harder37@gmail.com>
2019-01-19 11:22:26 +0100

    .

---
+++ src/main/kotlin/com/senacor/cap/service/message/ChatMessage.kt
@@ -0,0 +1,20 @@
+package com.senacor.cap.service.message
+
+import java.time.Instant
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+
+@Entity
+
+data class ChatMessage  (val channelId: String,
+                         val sender: String,
+                         val message: String,
+                         val creationTimestamp: Instant = Instant.now(),
+                         @Id @GeneratedValue
+                         val id: Long?=null)
+
+
+
+
