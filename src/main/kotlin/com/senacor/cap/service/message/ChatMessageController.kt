a13af7ef7ead63b174c3b9e2e49761604ee4088a
Max Johenneken <max.johenneken@anderscore.com>
2019-01-21 16:44:52 +0100

    fix controller

--- src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
+++ src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
@@ -8,15 +8,15 @@ import java.net.URI
 @RestController
 class ChatMessageController(val service: ChatMessageService) {
 
-    @GetMapping("/api/channels/{channelId}/messages/")
+    @GetMapping("/api/channels/{channelId}/messages")
     fun loadChatMessages(@PathVariable channelId: String): List<ChatMessage> {
         return service.loadChatMessages(channelId)
     }
 
-    @PostMapping("/api/channels/{channelId}/messages/")
+    @PostMapping("/api/channels/{channelId}/messages")
     fun newChatMessage(@PathVariable channelId: String, @RequestBody chatMessage: ChatMessage): ResponseEntity<ChatMessage> {
         val saveChatMessage = service.saveChatMessage(channelId, chatMessage.sender, chatMessage.message)
-        return ResponseEntity.created(URI.create("/api/channels/dev/messages/"+saveChatMessage.id)).build()
+        return ResponseEntity.created(URI.create("/api/channels/"+channelId+"/messages/"+saveChatMessage.id)).build()
     }
 
     @DeleteMapping("/api/channels/{channelId}/messages/{id}")
