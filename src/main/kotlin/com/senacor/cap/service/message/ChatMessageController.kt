4bc7dc7437833703a69030570c46d2a463a74913
Max Johenneken <max.johenneken@anderscore.com>
2019-01-21 15:35:41 +0100

    fix controller path variables

--- src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
+++ src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
@@ -1,23 +1,19 @@
 package com.senacor.cap.service.message
 
 
-import com.sun.jndi.toolkit.url.Uri
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.util.UriComponents
 import java.net.URI
-import java.util.*
 
 @RestController
 class ChatMessageController(val service: ChatMessageService) {
 
-    @GetMapping("/api/channels/dev/messages/")
-    fun loadChatMessages(s: String): List<ChatMessage> {
-        return service.loadChatMessages(s)
+    @GetMapping("/api/channels/{channelId}/messages/")
+    fun loadChatMessages(@PathVariable channelId: String): List<ChatMessage> {
+        return service.loadChatMessages(channelId)
     }
 
-    @PostMapping("/api/channels/dev/messages/{channelId}")
+    @PostMapping("/api/channels/{channelId}/messages/")
     fun newChatMessage(@PathVariable channelId: String, @RequestBody chatMessage: ChatMessage): ResponseEntity<ChatMessage> {
         var saveChatMessage = service.saveChatMessage(channelId, chatMessage.sender, chatMessage.message)
 
