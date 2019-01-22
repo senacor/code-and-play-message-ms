33623e7a2c0639317fdac0339642fd7e62f43054
ThorstenSm <thorsten.smeets@smail.inf.h-brs.de>
2019-01-22 11:26:46 +0100

    create classes

--- src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
+++ src/main/kotlin/com/senacor/cap/service/message/ChatMessageController.kt
@@ -2,30 +2,24 @@ package com.senacor.cap.service.message
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.util.UriComponentsBuilder
-import org.springframework.web.util.UriComponents
+import java.net.URI
 
-const val CHAT_MESSAGES_PATH = "/api/channels/{channelId}/messages"
+
 
 @RestController
-@RequestMapping(CHAT_MESSAGES_PATH)
-class ChatMessageController ( var service: ChatMessageService) {
+class ChatMessageController(val service: ChatMessageService) {
 
-    @GetMapping
-    fun loadChatMessages(@PathVariable("channelId") channelId : String): List<ChatMessage>{
-        Metrics.requests_total_get.increment()
+
+    @GetMapping("/api/channels/{channelId}/messages")
+    fun loadChatMessages(@PathVariable("channelId") channelId: String): List<ChatMessage>{
         val result = service.loadChatMessages(channelId)
         return result
     }
 
-    @PostMapping
-    fun newChatMessage(@PathVariable("channelId") channel: String, @RequestBody message: ChatMessage ): ResponseEntity<ChatMessage>{
-        Metrics.requests_total_post.increment()
-        val result = service.saveChatMessage(channel, message.sender, message.message)
+    @PostMapping("/api/channels/{channelId}/messages")
+    fun newChatMessage(@PathVariable("channelId") channelId: String, @RequestBody chatMessage: ChatMessage): ResponseEntity<ChatMessage>{
+        val result = service.saveChatMessage(channelId, chatMessage.sender, chatMessage.message)
 
-        val location:UriComponents = UriComponentsBuilder.newInstance().path(CHAT_MESSAGES_PATH)
-                .pathSegment(result.id!!.toString()).buildAndExpand(channel)
-        return ResponseEntity.created(location.toUri()).build()
+        return ResponseEntity.created(URI.create("/api/channels/${channelId}/messages/${result.id}")).build()
     }
-
 }
